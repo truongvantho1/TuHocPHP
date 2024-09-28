77daf28cc8baf70d8559b8e460b04b54a3e8e882
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-09-28 09:08:27 +0700

    update

---
+++ HTML/NhapCanhTamGiac.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form  action="TinhToan.php" method = "post">
+        Nhập cạnh thứ nhất: <input type="num" name = "canhthunhat" id="">
+        <br>
+        Nhập cạnh thứ hai: <input type="num" name = "canhthuhai" id="">
+        <br>
+        Nhập cạnh thứ ba: <input type="num" name = "canhthuba" id="">
+        <br>
+        <input type="submit" value="Tính toán">
+    </form>
+
+</body>
+</html>
---
+++ HTML/TinhToan.php
@@ -0,0 +1,15 @@
+<?php
+$a = $_POST["canhthunhat"];
+$b = $_POST["canhthuhai"];
+$c= $_POST["canhthuba"];
+if((($a + $b)>$c) || (($b + $c)>$a) || (($c + $b)>$b)) 
+{
+    echo "Day la tam giac<br>";
+    
+    $s= $a + $b + $c;
+    echo"chu vi tam giac  la:$s<br> ";
+    $dienTich = sqrt(($s / 2) * (($s / 2) - $a) * (($s / 2) - $b) * (($s / 2) - $c));
+    echo"dien tich tam giac là $dienTich";
+}else{
+    echo"Day khong phai la tam giac";
+}

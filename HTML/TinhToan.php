082dad64cd98ccf547002b3c16b850e6cfd139b4
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-09-28 09:20:19 +0700

    Update TinhToan.php

--- HTML/TinhToan.php
+++ HTML/TinhToan.php
@@ -5,8 +5,10 @@ $c= $_POST["canhthuba"];
 if((($a + $b)>$c) || (($b + $c)>$a) || (($c + $b)>$b)) 
 {
     echo "Day la tam giac<br>";
-    
     $s= $a + $b + $c;
+    echo"ba canh tam giac lan lượt là:<br> $a<br>";
+    echo"$b<br>";
+    echo"$c<br>";
     echo"chu vi tam giac  la:$s<br> ";
     $dienTich = sqrt(($s / 2) * (($s / 2) - $a) * (($s / 2) - $b) * (($s / 2) - $c));
     echo"dien tich tam giac là $dienTich";

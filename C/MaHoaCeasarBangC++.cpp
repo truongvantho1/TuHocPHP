b4f3cc0946d30c78728cb0c3b1c0374677d96a1e
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-09-28 08:00:18 +0700

    updating

---
+++ C/MaHoaCeasarBangC++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include <string>
+using namespace std;
+int main() {
+    char input[100];
+    int m;
+
+    cout << "Nhap doan ma can ma hoa: ";
+    cin.getline(input, 100);
+
+    cout << "Nhap khoa: ";
+    cin >> m;
+
+   	vector<char> result;
+
+    for (size_t i = 0; input[i] != '\0'; ++i) {
+        char Kytu = tolower(input[i]);
+
+        if (Kytu >= 'a' && Kytu <= 'z') {
+            int ViTri = Kytu - 'a';        
+            int newViTri = (ViTri + m) % 26; 
+            char newKytu = 'a' + newViTri;
+            result.push_back(newKytu);
+        } else {
+            result.push_back(input[i]);
+        }
+    }
+
+   	cout << "Day la ma ban dau:" << input << endl;
+    cout << "Khoa cua doan ma la: "<< m  << endl;
+    cout << "Day la ma da chuyen hoa : " << string(result.begin(), result.end()) << endl;
+
+    return 0;
+}

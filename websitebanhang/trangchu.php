031d88fbc583ebd27a9c1c2e7f0fe500977371dc
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-10-13 18:42:51 +0700

    update

---
+++ websitebanhang/trangchu.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Trangchu</title>
+        <link rel="stylesheet" href="trangchu.css">
+    </head>
+    <body>
+        <div class="header">
+            <h1>
+                <div class="tenshop">
+                    Pineapple Store Shoe
+                </div>
+                <div class="logotrangchu">
+                        <img src="shop.jpg" alt=""width="50px" height="50px">
+                </div>
+            </h1>
+            <h1>
+                <div class="contact-info">  
+                    Contact Us : phuc2333@gmail.com | Telephone Number:093509999 
+                </div>
+            </h1>
+        </div>
+        <div class="sidebar">
+            <a href="#">
+                <i class="fas fa-bars">
+                </i>
+                LIST
+            </a>
+            <a href="#">
+                <i class="fas fa-shoe-prints">
+                </i>
+                Shoe
+            </a>
+            <a href="#">
+                <i class="fas fa-socks">
+                </i>
+                Accessories
+            </a>
+            <a href="#">
+                <i class="fas fa-sliders-h">
+                </i>
+                Sandal
+            </a>
+            <a href="#">
+                <i class="fas fa-store">
+                </i>
+                Store
+            </a>
+        </div>
+        <div class="content">
+            <div class="row">
+                <div class="item">
+                    <img src="anhminhhoagiay.jfif" alt="" width="380px" height="180px">
+                    <img src="anhminhhoagiay1.jfif" alt=""width="380px"height="180px">
+                </div>
+            </div>
+            <div class="row">
+                <div class="item">
+                    <img src="giay1.jfif" alt="">   
+                    <img src="giay2.jfif" alt="">
+                    <img src="giay3.jfif" alt="">   
+                </div>
+            </div>
+            <div class="row">
+                <div class="price">
+                Giá sản phẩm:
+                Giá sản phẩm:
+                Giá sản phẩm:
+                </div>
+            </div>
+            <div class="row">
+                <div class="item">
+                    <img src="" alt="">   
+                </div>
+                <div class="item">
+                    <img src="" alt="">
+                </div>
+                <div class="item">
+                    <img src="" alt="">   
+                </div>
+            </div>
+            </div>
+        </div>
+
+    </body>
+</html>
